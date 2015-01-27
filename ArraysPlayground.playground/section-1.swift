7f208af48d9a3922428a568fee8768c52048f924
wiserkuo <wiserkuo@gmail.com>
2015-01-27 13:45:22 +0800

    array usage

---
+++ ArraysPlayground.playground/section-1.swift
@@ -0,0 +1,25 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var myArray:[String] = ["book","cat","dog"]
+myArray[0]
+
+myArray.count
+myArray.insert("dinosaur", atIndex: 0)
+
+myArray.count
+myArray[0]
+
+myArray.append("tree")
+myArray+=["elephant","truck"]
+
+myArray.count
+
+var mySecondArray:[Int]=[Int]()
+mySecondArray.append(3)
+mySecondArray.count
+mySecondArray[0]
+

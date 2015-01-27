dd60d6e2999190c0b2de9497d963ef476a618279
wiserkuo <wiserkuo@gmail.com>
2015-01-27 13:22:54 +0800

    random number

---
+++ RandPlayground.playground/section-1.swift
@@ -0,0 +1,15 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+arc4random_uniform(13)+1
+
+"test"
+
+var randomNumber = arc4random_uniform(13)+1
+
+String(format: "card%i", randomNumber) //$@ String , %i Int
+
+

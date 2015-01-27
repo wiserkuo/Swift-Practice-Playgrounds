6f767bcb38d592a763d2e389577ed32c6409bc98
wiserkuo <wiserkuo@gmail.com>
2015-01-27 13:55:50 +0800

    if statement

---
+++ ifPlayground.playground/section-1.swift
@@ -0,0 +1,19 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var a = 19
+
+if a<10{
+    println("hello")
+}
+else if a>10 && a<15{  //&&=and ||=or == = equal
+    println("in-between")
+}
+else {
+
+    println("goodbye")
+}
+
